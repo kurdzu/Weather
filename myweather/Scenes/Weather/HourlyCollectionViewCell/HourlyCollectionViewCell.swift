409f6421b48ded6d5c5f9026a904bc10da8a6956
kurdzu <o.katamadze@crystal.ge>
2023-03-14 13:09:41 +0400

    hourly collection view

---
+++ myweather/Scenes/Weather/HourlyCollectionViewCell/HourlyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HourlyCollectionViewCell.swift
+//  myweather
+//
+//  Created by omari katamadze on 14.03.23.
+//
+
+import UIKit
+
+class HourlyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
